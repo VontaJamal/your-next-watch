39e3cd850de936a32d5ef4527d1cdc773be55cbb
Vonta <djwstech@gmail.com>
2025-09-30 11:28:59 -0400

    display movies

--- src/components/MovieFinder/MovieFinder.data.tsx
+++ src/components/MovieFinder/MovieFinder.data.tsx
@@ -3,11 +3,17 @@ import {useQuery} from '@tanstack/react-query'
 import {authenticatedFetch} from '../../utils/client'
 
 export function MovieFinder() {
-  const {data: moviesData, isLoading, error} = useQuery<{movies: any[]}, Error>({
+  const {
+    data: moviesData,
+    isLoading,
+    error,
+  } = useQuery<{movies: any[]}, Error>({
     queryKey: ['movies'],
     queryFn: async () => {
       try {
-        const response = await authenticatedFetch('/movies')
+        const response = await authenticatedFetch(
+          '/movies',
+        )
         if (!response.ok) {
           throw new Error('Failed to fetch movies')
         }
@@ -18,7 +24,6 @@ export function MovieFinder() {
     },
   })
 
-  console.log('first', moviesData)
   return (
     <MovieFinderView data={moviesData} isLoading={isLoading} error={error} />
   )
